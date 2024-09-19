4b2074de457e63fa87c8b154672b5b8a8e28da9b
dlgkdud1118 <dlgkdud4057@naver.com>
2024-09-19 15:24:07 +0900

    Digital

---
+++ Digiclock/Digiclock/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Digiclock
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void QuitBtn_Click_1(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timelbl.Content = DateTime.Now.ToLongTimeString();
+            datelbl.Content = DateTime.Now.ToLongDateString();
+        }
+    }
+}
